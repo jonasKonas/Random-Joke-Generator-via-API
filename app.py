da0b881d7ff7261f7a54cf38a383d673486977df
Jonas Kiauleikis <jonas.kiauleikis@gmail.com>
2024-12-29 20:41:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import requests
 import os
 from dotenv import load_dotenv
 
-#Will load viariables from .env file
+# Will load viariables from .env file
+# No need to hardcode API keys
 load_dotenv()
 
 API_URL = f'https://api.api-ninjas.com/v1/jokes?'
-API_KEY = os.getenv("API_KEY")  # Replace with your API key
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    #joke = "What did one math book say to the other math book? We've got a lot of problems."
     joke = fetch_joke()
     return render_template("index.html", joke=joke)
 
@@ -22,10 +22,10 @@ def fetch_joke():
     response = requests.get(API_URL, headers={'X-Api-Key': API_KEY})
 
     if response.status_code == requests.codes.ok:
-        # Return the jokes as JSON
+        # Returns a joke
         return response.json()[0]["joke"]
     else:
-        # Handle errors
+        # If errors
         return jsonify({
             'error': response.status_code,
             'message': response.text
